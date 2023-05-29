e5882808d2a6a8db275816d40f68b42d2565cc34
biancatosetto <biancatosetto@gmail.com>
2023-05-29 20:55:13 -0300

    Leccion05
    Commit leccion 5 de Python

---
+++ TecnicaturaPy3/Leccion05/BD/v_i_registros.py
@@ -0,0 +1,22 @@
+import psycopg2 # Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor: # Cierra el recurso, sin necesidad de cursor.close()
+            sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s, %s, %s)'
+            valores = (
+                ('Carlos', 'Lara', 'clara@gmail.com'),
+                ('Marcos', 'Canto', 'mcanto@gmail.com'),
+                ('Marcelo', 'Cuenca', 'mcuenca@gmail.com')
+            )# Es una tupla de tuplas
+            cursor.executemany(sentencia, valores) # Ejecutamos la sentencia
+            # conexion.commit() - se utiliza para guardar los cambios en la base de datos, pero el with ya lo hace
+            registros_insertados = cursor.rowcount
+            print(f'Los registros insertados son: {registros_insertados}')
+
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
